a21f5b87448f7c92f7bb69ec0883b2749bf34034
Humad <humad@live.com>
2017-12-12 17:08:08 -0500

    added menu bar, added wish list view

---
+++ HCIProject4/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  HCIProject4
+//
+//  Created by Humad Syed on 12/12/17.
+//  Copyright © 2017 HCI. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var mainView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- HCIProject4/ViewController.swift
+++ HCIProject4/ViewController.swift
@@ -9,16 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var menuView: UIView!
+    var isMenuOpen = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        menuView.layer.shadowOpacity = 1
+    }
+    
+    @IBAction func presentHome(_ sender: Any) {
+        let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController")
         
+        self.present(homeViewController!, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func presentWishList(_ sender: Any) {
+        let wishListViewController = self.storyboard?.instantiateViewController(withIdentifier: "WishListViewController")
+        
+        self.present(wishListViewController!, animated: true, completion: nil)
     }
+    
+    @IBAction func openMenu(_ sender: Any) {
+        if (isMenuOpen) {
+            leadingConstraint.constant = -250
+        } else {
+            leadingConstraint.constant = 0;
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+        
+        isMenuOpen = !isMenuOpen
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
